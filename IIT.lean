f3a4ce1727c5c86f7f025714315f7f115a00322c
Jakob von Raumer <jakob@von-raumer.de>
2020-12-17 14:21:20 +0100

    maybe pre elab works now

--- IIT.lean
+++ IIT.lean
@@ -67,13 +67,15 @@ def declareInductiveTypes (views : Array InductiveView) (vars : Array Expr) (par
 
 def elabIIT (elems : Array Syntax) : CommandElabM Unit := do
   let views ← elems.mapM inductiveSyntaxToView
-  let pers ← liftTermElabM none (preElabViews #[] views) -- replace #[]
-  let params := pers.map PreElabResult.params
+  let (hrs, ctrs) ← liftTermElabM none (preElabViews #[] views) --TODO replace empty array
+  let params := hrs.map $ λ hr => hr.params
   let eits := erase $ pers.map PreElabResult.it
   let view0 := views[0]
   let ref := view0.ref
   runTermElabM view0.declName fun vars =>
      withRef ref $ declareInductiveTypes views vars params eits
+
+#check PreElabHeaderResult.toElabHeaderResult
 
 end IITElab
 
--- IIT/PreElab.lean
+++ IIT/PreElab.lean
@@ -42,34 +42,70 @@ partial def elabSingleHeader (view : InductiveView) : TermElabM ElabHeaderResult
                   localInsts := (← getLocalInstances),
                   params := params, type := type, view := view }
 
-structure PreElabResult where
-  it     : InductiveType
-  fVar   : Array Expr
-  params : Array Expr
+structure PreElabHeaderResult extends ElabHeaderResult where
+  fVar   : Expr
 
-partial def preElabViewsAux (views : Array InductiveView) (i : Nat) : TermElabM (Array PreElabResult) := do
+partial def preElabHeadersAux (views : Array InductiveView) (i : Nat) : TermElabM (Array PreElabHeaderResult) := do
   if (i >= views.size) then return #[]
-  let hr ← elabSingleHeader views[i]
-  let type ← mkForallFVars hr.params hr.type
-  withLCtx hr.lctx hr.localInsts do
-    let indFVar := mkFVar hr.view.declName --???
-    let ctors := [] --elabCtors indFVar hr.params hr
-    let indType := { name := hr.view.declName, type := type, ctors := ctors : InductiveType }
-    let per := { it := indType, params := hr.params, fVar := #[indFVar] : PreElabResult } --- [indFvar] ???
-    withLocalDeclD hr.view.declName type (λ _ => do let pers ← preElabViewsAux views (i + 1)
-                                                    pure $ #[per] ++ pers)
+  else
+    let hr ← elabSingleHeader views[i]
+    let type ← mkForallFVars hr.params hr.type
+    withLCtx hr.lctx hr.localInsts do
+      withLocalDeclD hr.view.declName type (λ indFVar => do
+        let hr := { toElabHeaderResult := { hr with type := type }, fVar := indFVar : PreElabHeaderResult }
+        let hrs ← preElabHeadersAux views (i + 1)
+        pure $ hrs.push hr)
+
+structure PreElabCtorResult extends Constructor where
+  fVar   : Expr
+
+partial def preElabConstrAuxAux (hr : PreElabHeaderResult) (view : InductiveView) (j : Nat) : TermElabM (Array PreElabCtorResult) := do
+  if (j >= view.ctors.size) then return #[]
+  else withRef view.ref do
+    let ctorView := view.ctors[j]
+    Term.elabBinders ctorView.binders.getArgs fun ctorParams => do
+    let type ← match ctorView.type? with
+      | none => throwError "constructor type must be specified"
+      | some ctorType => 
+        let type ← Term.elabTerm ctorType none
+        let resultingType ← getResultingType type
+        unless resultingType.getAppFn == hr.fVar do throwError! "unexpected constructor resulting type{indentExpr resultingType}"
+        unless (← isType resultingType) do throwError! "unexpected constructor resulting type, type expected"
+        let args := resultingType.getAppArgs
+        for i in [:hr.params.size] do
+          let param := hr.params[i]
+          let arg   := args[i]
+          unless (← isDefEq param arg) do throwError! "inductive datatype parameter mismatch"
+        pure type
+    let type ← mkForallFVars ctorParams type
+    let type ← mkForallFVars hr.params type
+    withLocalDeclD ctorView.declName type (λ ctorFVar => do
+      let cr := { name := ctorView.declName, type := type, fVar := ctorFVar : PreElabCtorResult }
+      let crs ← preElabConstrAuxAux hr view (j + 1)
+      return crs.push cr)
+
+partial def preElabConstrAux (hrs : Array PreElabHeaderResult) (views : Array InductiveView) (i : Nat) : TermElabM (Array (Array PreElabCtorResult)) := do
+  if (i >= views.size) then return #[]
+  withRef views[i].ref do
+    match hrs.get? i with
+    | none => throwError "empty header!"
+    | some hr => 
+      let prs ← preElabConstrAuxAux hr views[i] 0
+      let prss ← preElabConstrAux hrs views (i + 1)
+      return prss.push prs
 
 -- Adapted from Lean.Elab.Inductive
-def preElabViews (vars : Array Expr) (views : Array InductiveView) : TermElabM (Array PreElabResult) := do
+def preElabViews (views : Array InductiveView) : TermElabM (Array PreElabHeaderResult × Array (Array PreElabCtorResult)) := do
   let view0 := views[0]
   let scopeLevelNames ← Term.getLevelNames
   checkLevelNames views
   let allUserLevelNames := view0.levelNames
   let isUnsafe          := view0.modifiers.isUnsafe
   withRef view0.ref $ Term.withLevelNames allUserLevelNames do
-    let pers ← preElabViewsAux views 0
+    let hrs ← preElabHeadersAux views 0
+    let cts ← preElabConstrAux hrs views 0
     Term.synthesizeSyntheticMVarsNoPostponing
     --TODO other cosmetics
-    pure pers
+    pure (hrs, cts)
 
 end IIT
