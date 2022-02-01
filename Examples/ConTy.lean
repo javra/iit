ec07bd7089816414cf1941aa026f0f594a0dec83
Jakob von Raumer <jakob@von-raumer.de>
2022-02-01 11:13:12 +0100

    minimize conty example

--- Examples/ConTy.lean
+++ Examples/ConTy.lean
@@ -11,13 +11,9 @@ iit Ty : (Γ : Con) → Type
 | pi : ∀ (Γ : Con) (A : Ty Γ) (B : Ty (Con.ext Γ A)), Ty Γ
 
 iit_termination
-  · apply Ty.pi.m _ (A.ih _ _).1 (B.ih (Con.ext.r _ _ _ _ _ _ _ (A.ih _ _).2) _).1
-    repeat assumption 
-  · apply Ty.pi.r (A.r := (A.ih _ _).2) (B.r := (B.ih _ _).2)
-    repeat assumption
-  · apply Ty.pi.m _ (A.ih _ _).1 (B.ih (Con.ext.r _ _ _ _ _ _ _ (A.ih _ _).2) _).1
-    repeat assumption 
-  · apply Ty.pi.r (A.r := (A.ih _ _).2) (B.r := (B.ih _ _).2)
-    repeat assumption
+  repeat first 
+  | apply Ty.pi.m _ _ (B.ih (Con.ext.r _ _ _ _ _ _ _ (A.ih _ _).2) _).1
+  | apply Ty.pi.r (A.r := (A.ih _ _).2) (B.r := (B.ih _ _).2)
+  | assumption
 
 end
